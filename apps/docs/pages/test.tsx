182410f4c348d116fd11dd4dbe7941a2dd54388b
Kevin Østerkilde <kevin@oesterkilde.dk>
2023-08-02 20:51:42 +0200

    chore: cleanup

--- apps/docs/pages/test.tsx
+++
@@ -1,8 +0,0 @@
-import { parse, min, max, minLength, maxLength, email, object, string, number, boolean, date } from 'validathor';
-
-const TestPage = () => {
-  console.log(parse(object({}), {}))
-  return (<p>Test</p>)
-}
-
-export default TestPage
