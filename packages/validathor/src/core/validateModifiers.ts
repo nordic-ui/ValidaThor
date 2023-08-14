02317f925a1e5708dcc18addffa3898ca68b4a41
Kevin Østerkilde <kevin@oesterkilde.dk>
2023-08-14 22:27:37 +0100

    fix: build

--- packages/validathor/src/core/validateModifiers.ts
+++ packages/validathor/src/core/validateModifiers.ts
@@ -1,5 +1,7 @@
 import type { Modifier } from '../types'
 
 export const validateModifiers = (value: unknown, modifiers?: Modifier[]) => {
-  modifiers?.forEach((arg) => arg.validate(value))
+  // 🤷‍♂️ Idk why but I need to cast value to never to make it work
+  // `error occured in dts build`
+  modifiers?.forEach((arg) => arg.validate(value as never))
 }
