99fc775610814369e1c862b0ac9b195cd17415e1
Kevin Østerkilde <kevin@oesterkilde.dk>
2024-07-31 00:37:26 +0200

    feat(validathor): add tuple schema

---
+++ packages/validathor/src/schemas/tuple.ts
@@ -0,0 +1,27 @@
+import type { Parser } from '@/types'
+import { assert, TypeError } from '@/utils'
+import { ERROR_CODES } from '@/utils/errors/errorCodes'
+
+export const tuple = <T extends Parser<unknown>[]>(
+  schema: T,
+  message?: {
+    type_error?: string
+  },
+): Parser<T[]> => ({
+  name: 'tuple' as const,
+  parse: (value: unknown): T[] => {
+    assert(
+      Array.isArray(value),
+      new TypeError(message?.type_error || ERROR_CODES.ERR_TYP_0000.message()),
+    )
+
+    assert(
+      value.length === schema.length,
+      new TypeError(
+        ERROR_CODES.ERR_TYP_0000.message(message?.type_error || 'Tuple length mismatch'),
+      ),
+    )
+
+    return value.map((item, index) => schema[index].parse(item)) as T[]
+  },
+})
