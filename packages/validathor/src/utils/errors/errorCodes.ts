142677cc464b41ba315de6be168a86d96579178c
Kevin Østerkilde <kevin@oesterkilde.dk>
2024-07-31 00:16:57 +0200

    feat(validathor): update/align error codes

--- packages/validathor/src/utils/errors/errorCodes.ts
+++ packages/validathor/src/utils/errors/errorCodes.ts
@@ -64,13 +64,19 @@ const TYPE_ERROR_CODE: ErrorCodes<TypeErrorCode> = {
   // Enum type errors
   ERR_TYP_6000: {
     code: 'ERR_TYP_6000',
-    message: () => 'Expected an array',
+    message: () => 'Expected a tuple',
   },
 
   // Boolean type errors
   ERR_TYP_7000: {
     code: 'ERR_TYP_7000',
     message: () => 'Expected a boolean',
+  },
+
+  // Array type errors
+  ERR_TYP_8000: {
+    code: 'ERR_TYP_8000',
+    message: () => 'Expected an array',
   },
 }
 
@@ -80,16 +86,21 @@ const VALIDATION_ERROR_CODE: ErrorCodes<ValidationErrorCode> = {
     code: 'ERR_VAL_0000',
     message: () => 'Validation error',
   },
-  ERR_VAL_1010: {
-    code: 'ERR_VAL_1010',
+  ERR_VAL_0100: {
+    code: 'ERR_VAL_0100',
     message: () => 'Minimum length must be a positive number',
   },
-  ERR_VAL_1011: {
-    code: 'ERR_VAL_1011',
+  ERR_VAL_0200: {
+    code: 'ERR_VAL_0200',
     message: () => 'Maximum length must be a positive number',
   },
 
   // Number validation errors
+  ERR_VAL_1000: {
+    code: 'ERR_VAL_1000',
+    message: () => 'Expected a non-empty number',
+  },
+
   // String validation errors
   ERR_VAL_2000: {
     code: 'ERR_VAL_2000',
@@ -141,6 +152,22 @@ const VALIDATION_ERROR_CODE: ErrorCodes<ValidationErrorCode> = {
   // Enum validation errors
   ERR_VAL_6000: {
     code: 'ERR_VAL_6000',
+    message: () => 'Expected a non-empty input',
+  },
+
+  // Boolean validation errors
+  ERR_VAL_7000: {
+    code: 'ERR_VAL_7000',
+    message: () => 'Expected a boolean',
+  },
+
+  // Array validation errors
+  ERR_VAL_8000: {
+    code: 'ERR_VAL_8000',
+    message: () => 'Value must be an array',
+  },
+  ERR_VAL_8001: {
+    code: 'ERR_VAL_8001',
     message: () => 'Expected a non-empty input',
   },
 }
